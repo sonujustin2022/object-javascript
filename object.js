181056722b62de84c190463ad3ce37dbcbb3bec4
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-26 14:58:02 +0530

    object pratice started

---
+++ object.js
@@ -0,0 +1,15 @@
+let student = {
+    name:"bobin",
+    age:22,
+    place:"kollam",
+    attendence:56
+}
+
+
+// console.log(student);
+
+//print a specific keyvalue 
+
+console.log(student.name);
+
+console.log(student["age"]);
