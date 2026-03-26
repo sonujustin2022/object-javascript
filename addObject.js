805756e3d81f8fc037890251d0e426b36b3b2b0c
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-26 15:23:19 +0530

    add and Update Completed

---
+++ addObject.js
@@ -0,0 +1,13 @@
+let student = {
+    name:"bobin",
+    age:22,
+    place:"kollam",
+    attendence:56
+}
+
+console.log(student);
+
+//adding new keyvalue to object
+
+student["english_mark"] = 85;
+console.log(student);
---
+++ updateObj.js
@@ -0,0 +1,15 @@
+let customer = {
+    name:"bobin",
+    age:22,
+    exp:3,
+    spent:3200
+}
+
+
+console.log(customer);
+
+//updating Object
+
+customer["age"]=34;
+
+console.log(customer);
