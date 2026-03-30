c3df6228201471dfcaa1d430d7e77619e1f151ef
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-30 22:51:22 +0530

    task05 started

---
+++ task05.js
@@ -0,0 +1,14 @@
+const weatherData = [
+  { district: "Thrissur", temp: 32 },
+  { district: "Thiruvananthapuram", temp: 33 },
+  { district: "Kozhikode", temp: 31 },
+  { district: "Thrissur", temp: 28 },
+  { district: "Kozhikode", temp: 27 },
+  { district: "Ernakulam", temp: 26 },
+  { district: "Ernakulam", temp: 30 },
+  { district: "Thiruvananthapuram", temp: 29 },
+];
+
+// {thrissur:32,Thiruvananthapuram:33,Kozhikode:31,}
+
+// print district with its highest temperature
