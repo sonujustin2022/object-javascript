298e0081faa34509de429a5763821d5f0c378eb0
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-30 23:40:58 +0530

    task06 finished

--- task06.js
+++ task06.js
@@ -1,4 +1,28 @@
-// Find the first recursive character from the given pattern = "ABBCCBAD"
+// Find the first recursive character from the given pattern = "ABCCBAD"
 // ans: B
 
+let str = "ABBCCBAD";
 
+let strArr = str.split('');
+
+let recuschar = {};
+
+// strArr.forEach((eachChar)=>{
+//     if (Object.hasOwn(recuschar,eachChar)){
+
+//         console.log(eachChar, " is the first recursive character")
+//     }else{
+//         recuschar[eachChar] = 1;
+//     }
+// })
+
+
+for(let char of strArr){
+    if (Object.hasOwn(recuschar,char)){
+        console.log(char, " is the first recursive")
+        break;
+
+    }else {
+        recuschar[char]  = 1;
+    }
+}
