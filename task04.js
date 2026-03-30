4e220ac28015992ba423ef307dcbd20e83bd9728
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-30 22:49:28 +0530

    task04Finished

--- task04.js
+++ task04.js
@@ -3,7 +3,7 @@ const salesData = [
   { city: "Los Angeles", sales: 7000 },
   { city: "Chicago", sales: 6000 },
   { city: "New York", sales: 8000 },
-  { city: "Los Angeles", sales: 6500 },
+  { city: "Los Angeles", sales: 8500 },
   { city: "Chicago", sales: 9000 },
   { city: "Houston", sales: 4000 },
   { city: "Houston", sales: 5500 },
@@ -11,3 +11,20 @@ const salesData = [
 
 // Print each city with its highest sales value
 
+let highestSales = {};
+
+salesData.forEach((eachData)=>{
+    let currCity = eachData.city;
+    let currSales = eachData.sales;
+
+    if(Object.hasOwn(highestSales,currCity)){
+        if(currSales>highestSales[currCity]){
+             highestSales[currCity] = currSales;
+        }
+
+    }else {
+        highestSales[currCity] = currSales;
+    }
+});
+
+console.log(highestSales);
