45e42e88a3c59276cd5e5faf6bccc864abfbcf6a
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-31 22:13:21 +0530

    task08 started

---
+++ task08.js
@@ -0,0 +1,39 @@
+const products = [
+  { pid: 100, pName: "Apple", brand: "5G", price: 120000, display: "OLED" },
+  { pid: 101, pName: "Samsung", brand: "4G", price: 90000, display: "AMOLED" },
+  {
+    pid: 102,
+    pName: "OnePlus",
+    brand: "4G",
+    price: 75000,
+    display: "Fluid AMOLED",
+  },
+  { pid: 103, pName: "Pixel", brand: "3G", price: 85000, display: "OLED" },
+  {
+    pid: 104,
+    pName: "Xiaomi",
+    brand: "3G",
+    price: 40000,
+    display: "Super AMOLED",
+  },
+];
+
+// print product name only -
+
+// print all mobile details whose display is oled :
+
+
+console.log(`-------------------------------`);
+// print 3g mobile phone names :
+
+
+console.log(`-------------------------------`);
+// sort mobile based on price - sort
+
+
+console.log(`-------------------------------`);
+// print costly mobile price
+
+
+// print low cost mobile
+console.log(`-------------------------------`);
