773fc99cec0c376ed61d9e3445b3daa216746f7f
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-31 21:59:35 +0530

    task07 started

---
+++ task07.js
@@ -0,0 +1,13 @@
+const employeeAttendance = [
+  { employee: "John", status: "Present" },
+  { employee: "Mary", status: "Absent" },
+  { employee: "John", status: "Present" },
+  { employee: "Mary", status: "Present" },
+  { employee: "Steve", status: "Present" },
+  { employee: "Steve", status: "Absent" },
+  { employee: "Mary", status: "Present" },
+  { employee: "John", status: "Absent" },
+];
+
+// Question: Print how many days each employee was "Present".
+
