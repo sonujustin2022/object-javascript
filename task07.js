a2ec94ab66c09eeb5de4f01ad9bc4e455d6f5c11
sonujustin2022 <sonujustin6282@gmail.com>
2026-03-31 22:10:52 +0530

    task07 finished

--- task07.js
+++ task07.js
@@ -7,7 +7,31 @@ const employeeAttendance = [
   { employee: "Steve", status: "Absent" },
   { employee: "Mary", status: "Present" },
   { employee: "John", status: "Absent" },
+  { employee: "Ameer", status: "Absent" },
 ];
 
 // Question: Print how many days each employee was "Present".
 
+
+let empAttCount = {};
+
+employeeAttendance.forEach((eachAtt)=>{
+    let currEmp = eachAtt.employee;
+    let currStatus = eachAtt.status;
+    
+    if(Object.hasOwn(empAttCount,currEmp)){
+        if(currStatus == "Present" ){
+             empAttCount[currEmp] += 1;
+        }
+
+    }else{
+        if(currStatus == "Present" ){
+            empAttCount[currEmp] = 1;
+        }else{
+           empAttCount[currEmp] = 0; 
+        }
+    }
+
+});
+
+console.log(empAttCount);
